1c5179775dcec56106f6879d1fe368ccd3714e07
Christoph Herrmann <developer@dragonprojects.de>
2015-12-11 01:22:00 +0100

    Authentication depending on the environment variables 'DHTTP_AUTH_USER' and 'DHTTP_AUTH_PASSWORD'

---
+++ src/middleware.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const httpAuth = require('http-auth');
+
+if (process.env.DHTTP_AUTH_USER && process.env.DHTTP_AUTH_PASSWORD) {
+  let basic = httpAuth.basic(
+      { realm: require(process.cwd() + '/package.json').name },
+      (user, password, callback) => {
+        callback(user == process.env.DHTTP_AUTH_USER && password == process.env.DHTTP_AUTH_PASSWORD);
+      }
+  );
+  module.exports = httpAuth.connect(basic);
+} else {
+  module.exports = undefined;
+}
