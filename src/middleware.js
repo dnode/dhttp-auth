c878c80d1a784696d949d4a5f2ff455d3ad4e188
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-05-04 12:35:02 +0200

    Formatting

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@
 const httpAuth = require('http-auth');
 
 if (process.env.DHTTP_AUTH_USER && process.env.DHTTP_AUTH_PASSWORD) {
-  let basic = httpAuth.basic(
+  const basic = httpAuth.basic(
     { realm: require(process.cwd() + '/package.json').name },
     (user, password, callback) => {
       callback(user == process.env.DHTTP_AUTH_USER && password == process.env.DHTTP_AUTH_PASSWORD);
